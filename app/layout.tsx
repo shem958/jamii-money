4d0d718b688371848769801c0f6b0a2eb18c4bff
shem958 <shemgikunda37@gmail.com>
2025-10-12 12:33:13 +0300

    feat: wrap RootLayout with Redux Provider for state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,20 @@
 
 import { ReactNode } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 import theme from '../styles/theme';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {children}
-        </ThemeProvider>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );

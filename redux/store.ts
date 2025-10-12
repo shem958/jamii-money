9320c59acc5ddf695fd5aec9809284a0137c012c
shem958 <shemgikunda37@gmail.com>
2025-10-12 12:59:34 +0300

    feat: refactor store configuration to remove unused reducers and integrate authApi

--- redux/store.ts
+++ redux/store.ts
@@ -1,21 +1,13 @@
-'use client';
-
 import { configureStore } from '@reduxjs/toolkit';
-import { baseApi } from './api/baseApi';
-import authReducer from './slices/authSlice';
-import walletReducer from './slices/walletSlice';
+import { authApi } from '@/redux/api/authApi';
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
-        wallet: walletReducer,
-        [baseApi.reducerPath]: baseApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(baseApi.middleware),
-    devTools: process.env.NODE_ENV !== 'production',
+        getDefaultMiddleware().concat(authApi.middleware),
 });
 
-// ✅ Type helpers
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
